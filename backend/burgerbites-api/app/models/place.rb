a38d92e7eca3fbfb3e23ab9ad2efd360b8b3d4d6
08fdange <08fdange@gmail.com>
2020-09-15 03:10:56 -0400

    model relationships and cors initializer setup

--- backend/burgerbites-api/app/models/place.rb
+++ backend/burgerbites-api/app/models/place.rb
@@ -1,2 +1,6 @@
 class Place < ApplicationRecord
+    has_many :ratings
+    has_many :reviews
+    has_many users, through: :ratings
+    has_many users, through: :reviews
 end
--- backend/burgerbites-api/app/models/user.rb
+++ backend/burgerbites-api/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :ratings
+    has_many :reviews
+    has_many places, through: :ratings
+    has_many places, through: :reviews
 end
