478245ebc9733d5b626afae525f323c49a544d65
08fdange <08fdange@gmail.com>
2020-09-25 16:28:53 -0400

    Added validations and has_secure_password in User model

--- backend/burgerbites-api/app/models/user.rb
+++ backend/burgerbites-api/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     has_many :ratings
     has_many :reviews
+
+    #validations
+    validates_presence_of :name, :email, :password_digest
+    validates :email, uniqueness: true
+
+    #encrypt password
+    has_secure_password
 end
