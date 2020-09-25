520a0b14e7cf4e866a17043bdd1fca5aaaf52220
08fdange <08fdange@gmail.com>
2020-09-25 16:37:46 -0400

    Added to application file for autoload path

--- backend/burgerbites-api/config/application.rb
+++ backend/burgerbites-api/config/application.rb
@@ -33,5 +33,12 @@ module BurgerbitesApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #autoloads lib folder during production
+    config.eager_load_paths << Rails.root.join('lib')
+
+    #autoloads lib folder during development
+    config.autoload_paths << Rails.root.join('lib')
+    # [...]
   end
 end
